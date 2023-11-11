61ac9adf1ef6f1d0f19c30fc264075971ce5489b
omarordeche <omarordeche1997@gmail.com>
2023-11-11 10:56:32 +0100

    ajouter photo

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -53,7 +53,7 @@ return (
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect<ArrowRightCircle size={25} /> </button>  
             <Col xs={12} md={6} xl={5}>
-                {/* <img src={headerImg} alt="Heeadder Img" /> */}
+                { <img src={headerImg} alt="Heeadder Img" /> }
             </Col>
           </Col>
             </Row>
